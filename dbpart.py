e78ab7dfcb5413fc1ea79e8d0a5dabacc86fb99b
end <maslov0906@gmail.com>
2023-06-26 10:44:38 +0300

    2

--- dbpart.py
+++ dbpart.py
@@ -2,3 +2,20 @@ from pyArango.connection import *
 from information import username, password
 
 conn = Connection(username=username, password=password)
+db = conn.createDatabase(name="userinfo")
+db = conn["userinfo"]
+userslinksCollection = db.createCollection(name="UsersLinks")
+
+
+def new_user(user, link):
+    doc = userslinksCollection.createDocument()
+    doc['login'] = user
+    doc['links'] = [link]
+    doc._key = ''.join(user).lower()
+    doc.save()
+
+
+def link_addder(user, link):
+    doc = userslinksCollection[user]
+    doc['links'] = doc['links'].append(link)
+    doc.save()
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, UploadFile
+from dbpart import new_user, link_addder
 import shutil
 import os
 
@@ -8,16 +9,18 @@ app = FastAPI()
 @app.post("/userupload/{directoryname}")
 async def upload_file(directoryname: str, file: UploadFile):
     try:
+        link = f"users/{directoryname}/{file.filename}"
         os.mkdir(f"users/{directoryname}")
-        with open(f"users/{directoryname}/{file.filename}", "wb") as buffer:
+        with open(link, "wb") as buffer:
             shutil.copyfileobj(file.file, buffer)
-            return {f"{file.filename} successfully uploaded"}
+        new_links = new_user(directoryname, link)
+        return {f"{file.filename} successfully uploaded"}
     except FileExistsError:
-        if os.path.exists(f"users/{directoryname}/{file.filename}"):
+        if os.path.exists(link):
             msg = {f"{file.filename} successfully updated"}
         else:
+            add_link = link_addder(directoryname, link)
             msg = f"{file.filename} successfully uploaded"
-        with open(f"users/{directoryname}/{file.filename}", "wb") as buffer:
+        with open(link, "wb") as buffer:
             shutil.copyfileobj(file.file, buffer)
         return msg
-
